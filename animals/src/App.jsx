5469c9889ea972fcf662ac53476353d7587950c9
yerx <yer.x.xiong@gmail.com>
2020-10-01 12:56:47 -0400

    added routes to the details id pages and home page

--- animals/src/App.jsx
+++ animals/src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./components/SearchParams";
+import Details from "./components/Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div className="App">
-        <h1 id="something-important">Adopt me!</h1>
-        <SearchParams />
+        <header>
+          {/* when user clicks on adopt me it takes them back to the home page */}
+          <Link to="/">Adopt me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
