54be1e7750631306dde41d62dffe46e31be3e5b0
yerx <yer.x.xiong@gmail.com>
2020-10-01 06:28:35 -0400

    added a custom hook to handle dropdown elements

--- animals/src/components/SearchParams.jsx
+++ animals/src/components/SearchParams.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "../hooks/useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
@@ -15,6 +20,8 @@ const SearchParams = () => {
             onChange={(event) => setLocation(event.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button type="submit">Submit</button>
       </form>
     </div>
